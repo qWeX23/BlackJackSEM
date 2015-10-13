c61544899b43e27090de9f0fb26f3f969c9cd10a
Ben <bjc9001@gmail.com>
2015-10-13 18:41:05 -0500

    added packages, Card, Bootsrapper Class
    this should do us for this initial phase, obvoiusly fill the folders
    with more classes.  make sure your ide uses this repo as a src folder i
    had to do a lot of tinkering to figure how to create it and am willing
    to help everyone out (because its hella confusing)

---
+++ gui/Bootstrapper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author bjc90_000
+ *
+ */
+public class Bootstrapper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
