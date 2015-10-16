b62b25914c1c2057b985dd2e03cfd0d2bd58e76a
Ben <bjc9001@gmail.com>
2015-10-16 00:01:28 -0500

    did some more initinal GUI work still figuring it all out

--- gui/MenuWindow.java
+++ gui/MenuWindow.java
@@ -11,4 +11,25 @@ public class MenuWindow {
     private JButton LERNButton;
     private JButton creditsButton;
     private JButton HALPButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MenuWindow");
+        frame.setSize(500,400);
+        frame.setContentPane(new MenuWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MenuWindow() {
+        JFrame frame = new JFrame("MenuWindow");
+        frame.setSize(500,400);
+        frame.setContentPane(new MenuWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.pack();
+        frame.setVisible(true);
+
+
+
+    }
 }
