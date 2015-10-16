e2ffbb6fba4567ca55a57c02d0c237318cf9fd37
Ben <bjc9001@gmail.com>
2015-10-15 19:35:25 -0500

    began GUI work

---
+++ gui/MenuWindow.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by bjc90_000 on 10/15/2015.
+ */
+public class MenuWindow {
+    private JPanel panel1;
+    private JButton playBlackJackButton;
+    private JButton LERNButton;
+    private JButton creditsButton;
+    private JButton HALPButton;
+}
