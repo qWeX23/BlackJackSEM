141d80d8e084857c21084a7659e80dd62f1e7436
Ben <bjc9001@gmail.com>
2015-10-19 22:53:49 -0500

    Revert "Revert "Adding more backend""
    This reverts commit 83fe8fc3fac4d3bc9bbd437f28f123d2736eb44e.

--- backend/Hand.java
+++ backend/Hand.java
@@ -6,39 +6,81 @@ import java.util.ArrayList;
  * Created by bjc90_000 on 10/14/2015.
  */
 public class Hand {
+    private static final int ALT_ACE_VALUE = 11;
+    private static final int MAX_HAND = 21;
     ArrayList<Card> cards;
-    final int MAX_HAND=21;
-    public int calculateHandPower(){
-        boolean isBust =false;
-        boolean canGoHigher =true;
-        int totalPower=0;
+
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public int calculateHandPower() throws Exception {
+        boolean isBust = false;
+        //boolean canGoHigher =true;
+        int totalPower = 0;
 
         int max = calculateMaxPower(cards);
-        if(max<MAX_HAND)
+        int numAce = numAce();
+        //everything is ok we did not bust
+        if (max <= MAX_HAND)
             return max;
         else
+            isBust = true;//max is over bust can we do anything about it?
+        //its a bust and we cant do anything about it
+        if (numAce == 0) {
+            return max;
+        } else
+            //theres an ace!
+            if (numAce > 0) {
+                int currentUsedAces = numAce - 1;
+
+                while (isBust) {
+                    //add up ignoring aces
+                    for (Card c : cards) {
+                        if (c.getPower() != 1) {
+                            totalPower += c.getPower();
+                        }
+                    }
 
 
-        for(Card c:cards){
+                    totalPower += (currentUsedAces * ALT_ACE_VALUE);
+                    totalPower += ((numAce - currentUsedAces));
+
+                    if (totalPower <= MAX_HAND || currentUsedAces == 0) {
+                        return totalPower;
+                    } else {
+                        currentUsedAces--;
+                    }
+                }
+
+
+            }
+
+        throw  new Exception("something has gone wrong with calculationg this hand"+toString());
+
+    }
+
+
+    private int numAce() {
+        int num = 0;
+        for (Card c : cards) {
+            if (c.getPower() == 1) num++;
 
 
         }
-
-
-
-
-
-    return 99;
+        return num;
     }
 
-    private int calculateMaxPower(ArrayList<Card> cards) {
-        int power=0;
 
-        for(Card c:cards){
-            if(c.getPower()==1){
-                power+=11;
-            }else {
-                power+=c.getPower();
+    private int calculateMaxPower(ArrayList<Card> cards) {
+        int power = 0;
+
+        for (Card c : cards) {
+            if (c.getPower() == 1) {
+                power += 11;
+            } else {
+                power += c.getPower();
             }
         }
         return power;
