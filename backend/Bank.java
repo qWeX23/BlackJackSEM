1926e685c8046827c0b6128bf5ec02238109c9c9
jlb151 <jlb151@students.uwf.edu>
2015-10-21 11:57:34 -0500

    +bank.java

--- backend/Bank.java
+++ backend/Bank.java
@@ -3,5 +3,45 @@ package backend;
 /**
  * Created by bjc90_000 on 10/14/2015.
  */
-public class Bank {
+public class Bank 
+{
+	public static void main (String[]args)
+	{
+
+	  /** edited by jlb151 on 10/21/15.
+	  */
+	  int money =100.00;//currency
+	  int betAmount;
+  
+	  while(true)
+	  {
+		  do
+		  {
+			  System.print.out("Place bet amount!");
+			  betAmount = keyboard.hasNext();
+		  }
+		  
+		while(betAmount < 0 || betAmount > startingAmount)
+		{
+			if (betAmount == 0)
+				return (0);
+			else
+			{
+				Playerwins = PlayBlackJack();//toString		
+	  			money =  money + betAmount;
+			}
+	  			else if
+	  			{
+	  			PlayerLoses = money - betAmount;
+	  			money = money - betAmount;
+	  			}
+	  			else
+	  				if (money =0)
+	  					return(0);
+		}
+	  }
+	}
 }
+
+		  
+		
---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class PlayerName 
+{
+  private static String Playername;
+  Scanner System.in();
+  
+  
+}
