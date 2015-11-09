cce2c1ee8378a1f50dabfd9d377db09a0f8bb602
trm25 <trm25@students.uwf.edu>
2015-11-09 17:53:01 -0600

    Code clean-up and reformatting :)

--- gui/Bootstrapper.java
+++ gui/Bootstrapper.java
@@ -1,51 +1,16 @@
-/**
- * 
- */
 package gui;
 
 import Games.BJGame;
 import backend.Deck;
 import backend.Table;
 
-/**
- * @author bjc90_000
- *
- */
+//@author bjc90_000
 public class Bootstrapper {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("BJ TIMEE!\n Loading...");
-
-
 		Deck d = new Deck();
-
 		Table t = new Table(1,d);
 		new WindowController(t,new BJGame(t));
-
-		//MainMenu main = new MainMenu(t);
-
-		//System.out.println(t.toString());
-
-		//t.playBlackJack();
-
-
-
-
-
-
-
-
-
-
-
-
-		//System.out.println(d.drawCard());
-
 	}
-
 }
