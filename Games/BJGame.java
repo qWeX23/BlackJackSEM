5e5bedae0a5b61d0da1bffff82f250e6354df55f
Ben <bjc9001@gmail.com>
2015-11-12 10:38:39 -0600

    updated BJGAME

--- Games/BJGame.java
+++ Games/BJGame.java
@@ -2,8 +2,6 @@ package Games;
 
 import backend.Table;
 
-import java.util.concurrent.locks.Lock;
-
 /**
  * Created by bjc90_000 on 11/6/2015.
  */
@@ -12,7 +10,6 @@ public class BJGame extends GameCoordinator {
     public BJGame(Table t) {
         this.table = t;
     }
-
 
 
     @Override
@@ -24,32 +21,27 @@ public class BJGame extends GameCoordinator {
 
             System.out.println("Place Bet...");
             canBet = true;
-            flagUpdate();
+
             while (canBet) Thread.sleep(10);//System.out.println("doing nothing waiting for the bet");
-            flagUpdate();
+
             //Deal Cards to Dealer and Player
-
-
             table.reset();
             this.reset();
-
             System.out.println("INITiAL DEAL \n" + table.toString());
             gameState = "Initial Deal";
 
-            flagUpdate();
+
             //signalGUI update
             //Players Turn
             System.out.println("YOUR TURN \n" + table.toString());
             gameState = "Your Turn";
             while (!wantsStand && !table.playerisBust()) {
-                Thread.sleep(10);
                 //System.out.println("Stuck Waiting for stand");
 
                 if (wantsHit) {
                     table.playerHit();
                     wantsHit = false;
                     System.out.println("YOUR TURN \n" + table.toString());
-                    flagUpdate();
                 }
             }
             //Dealer's Turn
@@ -57,14 +49,12 @@ public class BJGame extends GameCoordinator {
                 table.dealersTurn();
                 System.out.println("DEALER TURN \n" + table.toString());
                 gameState = "Dealer Turn";
-                flagUpdate();
 
             } else {
                 System.out.println("DEALER WINS by player bustl");
                 dealerWins = true;
                 winnerDetermined = true;
                 gameState = "Dealer Wins by Player Bust";
-                flagUpdate();
             }
             //Determine Winner
             if (table.dealerisBust()) {
@@ -72,24 +62,33 @@ public class BJGame extends GameCoordinator {
                 gameState = "Player Wins by Dealer Bust";
 
                 playerWins = true;
-                flagUpdate();
 
             } else if (!table.playerisBust() && !table.dealerisBust()) {
 
-                //TODO check for and give priority to blackjack
+                if (table.playerBJ() || table.dealerBJ()) {
+
+                    if (table.playerBJ() && !table.dealerBJ()) {
+
+                        gameState ="BJ!!";
+
+                    } else if (table.dealerBJ() && !table.dealerBJ()) {
+
+                    }
+
+                }else
+
                 if (table.playerHandPower() == table.dealerHandPower()) {
                     gameState = "PUSH";
-                    flagUpdate();
-                } else if (table.playerHandPower() > table.dealerHandPower()) {
+                } else
+
+                if (table.playerHandPower() > table.dealerHandPower()) {
                     playerWins = true;
                     System.out.println("PLAYER WINS by larger hand");
                     gameState = "Player Wins by Larger Hand";
-                    flagUpdate();
                 } else {
                     System.out.println("DEALER WINS by larger hand");
                     gameState = "Dealer Wins by Larger Hand";
                     dealerWins = true;
-                    flagUpdate();
                 }
             }
 
