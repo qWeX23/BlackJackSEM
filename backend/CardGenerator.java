bdb75721cf660b1221d2cc16d09ae60d5a973463
Ben <bjc9001@gmail.com>
2015-11-12 22:45:24 -0600

    fixes #26 NEW CARDS.BJ FILE +++++++++++++
    sometimes github freaks out about this so let me know if it donst work

--- backend/CardGenerator.java
+++ backend/CardGenerator.java
@@ -14,46 +14,35 @@ import static javax.imageio.ImageIO.read;
  */
 public class CardGenerator {
 
-    public static final String[] SUITS = {"Spades","Diamonds","Clubs","Hearts"};
-    public static final String[] RANKS = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    public static final String[] SUITS = {"Spades", "Diamonds", "Clubs", "Hearts"};
+    public static final String[] RANKS = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Card[] c = new Card[52];
         try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Cards.bj"));
-           ImageIcon front;
-            int power,index=0;
-            ImageIcon  back = new ImageIcon(read(new File("C:/Users/bjc90_000/Downloads/JackBlack.jpg")));
+            ImageIcon front;
+            int power, index = 0;
+            ImageIcon back = new ImageIcon(read(new File("C:/Users/bjc90_000/Downloads/JackBlack.jpg")));
 
-            for(String s : SUITS){
+            for (String s : SUITS) {
                 power = 1;
-                for(String r:RANKS) {
-                    front = new ImageIcon(read(new File("C:/Users/bjc90_000/Desktop/BlackJackSEM/Resources/PNG-cards-1.3/" + r.toLowerCase() + "_of_" + s.toLowerCase()+".png")));
-
-
-                    c[index]= new Card(r,s,power,front,back);
-                    if(power<10)power++; index++;
-
-
+                for (String r : RANKS) {
+                    if (r.equals("Jack") || r.equals("Queen") || r.equals("King"))
+                        front = new ImageIcon(read(new File("C:/Users/bjc90_000/Documents/GitHub/BlackJackSEM/Resources/PNG-cards-1.3/" + r.toLowerCase() + "_of_" + s.toLowerCase() + "2.png")));
+                    else
+                        front = new ImageIcon(read(new File("C:/Users/bjc90_000/Documents/GitHub/BlackJackSEM/Resources/PNG-cards-1.3/" + r.toLowerCase() + "_of_" + s.toLowerCase() + ".png")));
+                    c[index] = new Card(s, r, power, front, back);
+                    System.out.println("Created"+c[index].toString());
+                    if (power < 10) power++;
+                    index++;
                 }
             }
-
             out.writeObject(c);
             out.close();
-
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-    }
-
-
-
-
-    public CardGenerator() {
     }
 }
